9ea107c150f73a063b441026200de2103905769d
Elijah Malaby <emalaby@digitalglobe.com>
2017-03-17 14:52:06 -0400

    Added color to verticies and passed through geometry shader.

--- bezier.lisp
+++ bezier.lisp
@@ -9,15 +9,16 @@
 (defparameter *running* nil)
 
 
-(defun-g vert ((point :vec3))
-  (v! point 1.0))
+(defun-g vert ((point g-pc))
+  (values (v! (pos point) 1.0)
+          (col point)))
 
 (defun-g geom ()
   (setq gl-position (v! 1.0 1.0 1.0 1.0))
   (emit-vertex)
   (values))
 
-(def-glsl-stage geom-glsl (&context :330 :geometry)
+(def-glsl-stage geom-glsl (("color" (:vec4 (*))) &context :330 :geometry)
   "
 layout(lines) in;
 layout(line_strip, max_vertices=5) out;
@@ -28,37 +29,38 @@ layout(line_strip, max_vertices=5) out;
 void main()
 {
   gl_Position = vec4( gl_in[0].gl_Position.xyz, 1.0 );
-
+  color_out = color[0];
 	EmitVertex();
   gl_Position = vec4( gl_in[1].gl_Position.xyz, 1.0 );
+  color_out = color[1];
 
 	EmitVertex();
 
+  EndPrimitive();
   gl_Position = vec4( -gl_in[0].gl_Position.x,
                        gl_in[0].gl_Position.yz,
                        1.0 );
-
-  EndPrimitive();
+  color_out = color[1];
 
 	EmitVertex();
   gl_Position = vec4( -gl_in[1].gl_Position.x,
                        gl_in[1].gl_Position.yz,
                        1.0 );
-
+  color_out = color[0];
 	EmitVertex();
 
 	EndPrimitive();
-}" ())
+}" (("color_out" :vec4)))
 
 
 
-(defun-g frag ()
-  (v! 1.0 1.0 0.0 1.0))
+(defun-g frag ((color :vec4))
+  color)
 
 (def-g-> main-prog (:lines)
-  :vertex #'(vert :vec3)
-  :geometry #'(geom-glsl)
-  :fragment #'(frag))
+  :vertex #'(vert g-pc)
+  :geometry #'(geom-glsl (:vec4 (*)))
+  :fragment #'(frag :vec4))
 
 (defun step-demo ()
   (clear)
@@ -68,9 +70,9 @@ void main()
 (defun run-loop ()
   (setf *running* t)
   (setf *lines* (make-gpu-array
-                 (list (v! -1.0 1.0 -0.1)
-                       (v! 1.0 -1.0 -0.1))
-                 :element-type :vec3
+                 (list (list (v! -1.0 1.0 -0.1) (v! 0.0 0.0 1.0 1.0))
+                       (list (v! 1.0 -1.0 -0.1) (v! 0.0 1.0 0.0 1.0)))
+                 :element-type 'g-pc
                  :dimensions 2))
   (setf *lines-stream* (make-buffer-stream *lines* :retain-arrays t))
   (gl:depth-func :lequal)
