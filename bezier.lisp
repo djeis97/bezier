ba8089ac8cb537ecfd739863712cc3c603eb9d9f
Elijah Malaby <emalaby@digitalglobe.com>
2017-03-24 17:09:25 -0400

    Added in untested tessellation shader.

--- bezier.lisp
+++ bezier.lisp
@@ -4,8 +4,10 @@
 
 ;;; "bezier" goes here. Hacks and glory await!
 
-(defparameter *lines* nil)
-(defparameter *lines-stream* nil)
+(defparameter *geom-lines* nil)
+(defparameter *geom-lines-stream* nil)
+(defparameter *tess-lines* nil)
+(defparameter *tess-lines-stream* nil)
 (defparameter *running* nil)
 
 (defstruct-g g-poc
@@ -18,10 +20,24 @@
           (v! (control point) 1.0)
           (col point)))
 
-(defun-g geom ()
-  (setq gl-position (v! 1.0 1.0 1.0 1.0))
-  (emit-vertex)
-  (values))
+(defun-g vert ((point g-pc))
+  (values (v! (pos point) 1.0)))
+
+(def-glsl-stage tess-eval (&context :400 :tess-eval)
+  "
+layout(isolines) in;
+
+void main()
+{
+  color_out = vec4(1.0, 0.0, 0.0, 1.0);
+  float OneMinusT = 1.0 - gl_TessCoord.x;
+  float b0 = OneMinusT*OneMinusT*OneMinusT;
+  float b1 = 3.0*t*OneMinusT*OneMinusT;
+  float b2 = 3.0*t*t*OneMinusT;
+  float b3 = t*t*t;
+  gl_Position = b0*gl_in[0].gl_Position + b1*gl_in[1].gl_Position + b2*gl_in[2].gl_Position + b3*gl_in[3].gl_Position;
+}"
+  (("color_out" :vec4)))
 
 (def-glsl-stage geom-glsl (("control" (:vec4 (*))) ("color" (:vec4 (*)))
                                                    &uniform
@@ -34,7 +50,6 @@ layout(line_strip, max_vertices=20) out;
 
 vec3 evaluateBezierPosition( vec3 v[4], float t )
 {
-    vec3 p;
     float OneMinusT = 1.0 - t;
     float b0 = OneMinusT*OneMinusT*OneMinusT;
     float b1 = 3.0*t*OneMinusT*OneMinusT;
@@ -67,7 +82,6 @@ void main()
 }" (("color_out" :vec4)))
 
 
-
 (defun-g frag ((color :vec4))
   color)
 
@@ -76,19 +90,35 @@ void main()
   :geometry #'(geom-glsl (:vec4 (*)) (:vec4 (*)))
   :fragment #'(frag :vec4))
 
+(def-g-> tess-prog (:patches)
+  :vertex #'(vert g-pc)
+  :tess-eval #'(tess-eval)
+  :fragment #'(frag :vec4))
+
 (defun step-demo ()
   (clear)
-  (map-g #'main-prog *lines-stream* :transform (m4:scale (v! 1.0 1.0 1.0)) :detail 20)
+  (map-g #'main-prog *geom-lines-stream* :transform (m4:scale (v! 1.0 1.0 1.0)) :detail 20)
+  (map-g #'tess-prog *tess-lines-stream*)
   (swap))
 
 (defun run-loop ()
   (setf *running* t)
-  (setf *lines* (make-gpu-array
+  (setf *geom-lines* (make-gpu-array
                  (list (list (v! -1.0 1.0 -0.1) (v! -1.0 0.0 0.0) (v! 0.0 0.0 1.0 1.0))
                        (list (v! 1.0 -1.0 -0.1) (v! 1.0 0.0 0.0) (v! 0.0 1.0 0.0 1.0)))
                  :element-type 'g-poc
                  :dimensions 2))
-  (setf *lines-stream* (make-buffer-stream *lines* :retain-arrays t))
+  (setf *geom-lines-stream* (make-buffer-stream *geom-lines* :retain-arrays t))
+  (setf *tess-lines* (make-gpu-array
+                      (list (list (v! -1.0 1.0 -0.1) (v! 0.0 0.0 1.0 1.0))
+                            (list (v! -1.0 0.0 0.0) (v! 0.0 0.0 1.0 1.0))
+                            (list (v! 1.0 0.0 0.0) (v! 0.0 1.0 0.0 1.0))
+                            (list (v! 1.0 -1.0 -0.1) (v! 0.0 1.0 0.0 1.0)))
+                      :element-type 'g-pc
+                      :dimensions 4))
+  (setf *tess-lines-stream* (make-buffer-stream *tess-lines* :retain-arrays t))
+  (%gl:patch-parameter-i :patch-vertices 4)
+  (%gl:patch-parameter-fv :patch-default-outer-level (c-array-pointer (make-c-array '(1.0 4.0 0.0 0.0))))
   (gl:depth-func :lequal)
   (loop :while (and *running* (not (shutting-down-p))) :do
     (livesupport:continuable
